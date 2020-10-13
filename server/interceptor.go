e1fb9278fc1e1827f0960c92b0aeebe5216b8e8d
eldius <eldiosantos@gmail.com>
2020-10-13 16:23:01 -0300

    Added a prototype of CORS interceptor (still needs to be refined)

--- server/interceptor.go
+++ server/interceptor.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Eldius/auth-server-go/logger"
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
 // ContextKey is used for context.Context value. The value requires a key that is not primitive type.
@@ -24,5 +25,20 @@ func RequestIdInterceptor(next http.Handler) http.Handler {
 		log.WithField("req", id.String()).Debugf("Incomming request %s %s %s %s", r.Method, r.RequestURI, r.RemoteAddr, id.String())
 		next.ServeHTTP(w, r)
 		log.WithField("req", id.String()).Debugf("Finished handling http req. %s", id.String())
+	})
+}
+
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := logger.Logger()
+		log.WithFields(logrus.Fields{
+			"origin": r.Header.Get("Origin"),
+			"dest":   r.URL.String(),
+			"method": r.Method,
+		}).Info("CORS")
+
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		next.ServeHTTP(w, r)
 	})
 }
