cb2458cd9ec3f301b8949a7f3acfc7029c6c1894
Eldio Santos Junior <eldiosantos@gmail.com>
2020-10-15 02:29:53 +0000

    Added some variables to config build info

--- config/config.go
+++ config/config.go
@@ -2,6 +2,21 @@ package config
 
 import "github.com/spf13/viper"
 
+var (
+	version string
+	buildDate string
+)
+
+// GetVersion returns app version
+func GetVersion() string {
+	return version
+}
+
+// GetBuildDate returns app build date
+func GetBuildDate() string {
+	return buildDate
+}
+
 // GetDBURL returns the database url
 func GetDBURL() string {
 	return viper.GetString("app.database.url")
