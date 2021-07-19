01c82a3697ec236d56cb2c8204c42b95e2812a89
eldius <eldiosantos@gmail.com>
2021-07-19 15:36:35 -0300

    Starting the frontend code...

---
+++ static/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Message Server
+        </p>
+      </header>
+      <div>
+        <p>
+          Starting the frontend code.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
